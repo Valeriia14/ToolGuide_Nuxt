f1a7f4e26716ca6b50340cd8c00ab546a71278fa
Valeriia14 <valeriiadesign14@gmail.com>
2023-12-06 15:57:27 +0200

    update the nav bar and top meun bar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,4 +1,12 @@
 <template>
+  <!--
+    This example requires updating your template:
+
+    ```
+    <html class="h-full bg-white">
+    <body class="h-full">
+    ```
+  -->
   <div>
     <TransitionRoot as="template" :show="sidebarOpen">
       <Dialog
@@ -15,7 +23,7 @@
           leave-from="opacity-100"
           leave-to="opacity-0"
         >
-          <div class="fixed inset-0 bg-black-900/80" />
+          <div class="fixed inset-0 bg-gray-900/80" />
         </TransitionChild>
 
         <div class="fixed inset-0 flex">
@@ -47,15 +55,73 @@
                     @click="sidebarOpen = false"
                   >
                     <span class="sr-only">Close sidebar</span>
-                    <!-- <XMarkIcon class="h-6 w-6 text-white" aria-hidden="true" /> -->
+                    <XMarkIcon class="h-6 w-6 text-white" aria-hidden="true" />
                   </button>
                 </div>
               </TransitionChild>
               <!-- Sidebar component, swap this element with another sidebar if you like -->
               <div
-                class="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4 ring-1 ring-white/10"
+                :class="[
+                  theme ? 'bg-dark-primary' : 'bg-white',
+                  'flex grow flex-col gap-y-5 overflow-y-auto px-6 pb-4 ring-1 ring-white/10',
+                ]"
               >
-                <div class="flex h-16 shrink-0 items-center"></div>
+                <div class="flex h-16 shrink-0 items-center">
+                  <h5 class="text-dark-secondary font-bold text-3xl">
+                    Tool &nbsp;<span
+                      :class="[theme ? 'text-white' : 'text-black']"
+                      >Glide</span
+                    >
+                  </h5>
+                </div>
+                <nav class="flex flex-1 flex-col">
+                  <ul role="list" class="flex flex-1 flex-col gap-y-7">
+                    <li>
+                      <ul role="list" class="-mx-2 space-y-1">
+                        <li v-for="item in navigation" :key="item.name">
+                          <a
+                            :href="item.href"
+                            :class="[
+                              theme
+                                ? 'text-white hover:bg-dark-light'
+                                : 'text-gray-400 hover:text-black hover:bg-light-light',
+                              'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold',
+                            ]"
+                          >
+                            <component
+                              :is="item.icon"
+                              class="h-6 w-6 shrink-0"
+                              aria-hidden="true"
+                            />
+                            {{ item.name }}
+                            <span
+                              v-if="item.count"
+                              class="ml-auto w-[25px] whitespace-nowrap rounded-md bg-red-500 px-1 py-1 text-center text-xs font-sans text-white"
+                              aria-hidden="true"
+                              >{{ item.count }}</span
+                            >
+                          </a>
+                        </li>
+                      </ul>
+                    </li>
+
+                    <li class="mt-auto">
+                      <a
+                        @click="changeTheme"
+                        href="#"
+                        :class="[
+                          theme
+                            ? 'hover:bg-dark-light hover:text-white'
+                            : 'hover:bg-light-light hover:text-black',
+                          'group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400',
+                        ]"
+                      >
+                        <Setting class="h-6 w-6 shrink-0" aria-hidden="true" />
+                        Setting
+                      </a>
+                    </li>
+                  </ul>
+                </nav>
               </div>
             </DialogPanel>
           </TransitionChild>
@@ -65,12 +131,21 @@
 
     <!-- Static sidebar for desktop -->
     <div
-      class="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col "
+      class="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col"
     >
       <!-- Sidebar component, swap this element with another sidebar if you like -->
-      <div :class="[theme? 'bg-gray-900' : 'bg-white', 'flex grow flex-col gap-y-5 overflow-y-auto px-6 pb-4']">
-        <div class="flex h-16 shrink-0 items-center justify-center">
-          <h5 class="text-purple-600 font-bold text-3xl">Tool &nbsp;<span :class="[theme ? 'text-white' :'text-black' ]">Glide</span></h5>
+      <div
+        :class="[
+          theme ? 'bg-dark-primary' : 'bg-white',
+          'flex grow flex-col gap-y-5 overflow-y-auto px-6 pb-4',
+        ]"
+      >
+        <div class="flex h-16 shrink-0 items-center">
+          <h5 class="text-dark-secondary font-sans font-extrabold text-3xl">
+            Tool &nbsp;<span :class="[theme ? 'text-white' : 'text-black']"
+              >Glide</span
+            >
+          </h5>
         </div>
         <nav class="flex flex-1 flex-col">
           <ul role="list" class="flex flex-1 flex-col gap-y-7">
@@ -81,9 +156,9 @@
                     :href="item.href"
                     :class="[
                       theme
-                        ? 'bg-gray-900 text-white'
-                        : 'text-gray-400 hover:text-white',
-                      'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold hover:bg-purple-600',
+                        ? 'text-white hover:bg-dark-light'
+                        : 'text-gray-400 hover:text-black hover:bg-light-light',
+                      'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold',
                     ]"
                   >
                     <component
@@ -92,17 +167,30 @@
                       aria-hidden="true"
                     />
                     {{ item.name }}
+                    <span
+                      v-if="item.count"
+                      class="ml-auto w-[25px] whitespace-nowrap rounded-md bg-red-500 px-1 py-1 text-center text-xs font-sans text-white"
+                      aria-hidden="true"
+                      >{{ item.count }}</span
+                    >
                   </a>
                 </li>
               </ul>
             </li>
+
             <li class="mt-auto">
               <a
-                href="#"
-                class="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-purple-600 hover:text-white"
                 @click="changeTheme"
+                href="#"
+                :class="[
+                  theme
+                    ? 'hover:bg-dark-light hover:text-white'
+                    : 'hover:bg-light-light hover:text-black',
+                  'group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400',
+                ]"
               >
-                Change Theme
+                <Setting class="h-6 w-6 shrink-0" aria-hidden="true" />
+                Setting
               </a>
             </li>
           </ul>
@@ -112,7 +200,10 @@
 
     <div class="lg:pl-72">
       <div
-        :class="[theme? 'bg-gray-900' : 'bg-white', 'sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200  px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8']"
+        :class="[
+          theme ? 'bg-dark-primary' : 'bg-white',
+          'sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8',
+        ]"
       >
         <button
           type="button"
@@ -120,18 +211,20 @@
           @click="sidebarOpen = true"
         >
           <span class="sr-only">Open sidebar</span>
+          <Bars3Icon class="h-6 w-6" aria-hidden="true" />
         </button>
 
         <!-- Separator -->
         <div class="h-6 w-px bg-gray-900/10 lg:hidden" aria-hidden="true" />
 
-        <div class="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
+        <div class="flex flex-1 gap-x-4 self-stretch lg:gap-x-6 justify-end">
           <div class="flex items-center gap-x-4 lg:gap-x-6">
             <button
               type="button"
               class="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500"
             >
               <span class="sr-only">View notifications</span>
+              <BellIcon class="h-6 w-6" aria-hidden="true" />
             </button>
 
             <!-- Separator -->
@@ -155,6 +248,10 @@
                     aria-hidden="true"
                     >Tom Cook</span
                   >
+                  <ChevronDownIcon
+                    class="ml-2 h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
                 </span>
               </MenuButton>
               <transition
@@ -166,7 +263,7 @@
                 leave-to-class="transform opacity-0 scale-95"
               >
                 <MenuItems
-                  class="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-gray-900 py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none"
+                  class="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none"
                 >
                   <MenuItem
                     v-for="item in userNavigation"
@@ -177,7 +274,7 @@
                       :href="item.href"
                       :class="[
                         active ? 'bg-gray-50' : '',
-                        'block px-3 py-1 text-sm leading-6 text-red-900',
+                        'block px-3 py-1 text-sm leading-6 text-gray-900',
                       ]"
                       >{{ item.name }}</a
                     >
@@ -188,9 +285,11 @@
           </div>
         </div>
       </div>
-
       <main
-        :class="[ theme ? 'bg-gray-900':'bg-white','h-screen text-black border border-gray-200']"
+        :class="[
+          theme ? 'bg-dark-strong' : 'bg-light-strong',
+          'h-screen text-black',
+        ]"
       >
         <div class="w-full h-full">
           <!-- Your content -->
@@ -202,12 +301,6 @@
 
 <script setup>
 import { ref } from "vue";
-import Dashboard from "@/components/Icons/Dashboard.vue";
-import Discover from "@/components/Icons/Discover.vue";
-import Tool from "@/components/Icons/Tool.vue";
-import Support from "@/components/Icons/Support.vue";
-import Resources from "@/components/Icons/Resources.vue";
-import Tips from "@/components/Icons/Tips.vue";
 import {
   Dialog,
   DialogPanel,
@@ -219,12 +312,39 @@ import {
   TransitionRoot,
 } from "@headlessui/vue";
 
+import Dashboard from "@/components/Icons/Dashboard.vue";
+import Discover from "@/components/Icons/Discover.vue";
+import Tool from "@/components/Icons/Tool.vue";
+import Support from "@/components/Icons/Support.vue";
+import Resources from "@/components/Icons/Resources.vue";
+import Tips from "@/components/Icons/Tips.vue";
+import Setting from "@/components/Icons/Setting.vue";
+
+import {
+  Bars3Icon,
+  BellIcon,
+  ChartPieIcon,
+  Cog6ToothIcon,
+  XMarkIcon,
+} from "@heroicons/vue/24/outline";
+import { ChevronDownIcon, MagnifyingGlassIcon } from "@heroicons/vue/20/solid";
+
 const navigation = [
   { name: "Dashboard", href: "#", icon: Dashboard, current: true },
   { name: "Discover", href: "#", icon: Discover, current: false },
-  { name: "Tool", href: "#", icon: Tool, current: false },
+  {
+    name: "Tool",
+    href: "#",
+    icon: Tool,
+    current: false,
+    subNavs: [
+      { name: "E-mail Timer", href: "#" },
+      { name: "Notification Bell", href: "#" },
+      { name: "EVideo Pop", href: "#" },
+    ],
+  },
   { name: "Support", href: "#", icon: Support, current: false },
-  { name: "Resources", href: "#", icon: Resources, current: false },
+  { name: "Resources", href: "#", icon: Resources, current: false, count: 5 },
   { name: "Tips & Tricks", href: "#", icon: Tips, current: false },
 ];
 
@@ -237,7 +357,6 @@ const sidebarOpen = ref(false);
 </script>
 
 <script>
-import { mapMutations } from "vuex";
 export default {
   name: "NavBar",
   data() {
@@ -247,22 +366,15 @@ export default {
   },
   methods: {
     changeTheme() {
+      console.log("aaa");
       this.theme = !this.theme;
+    },
+
+    shouldShowItem(item) {
+      // You can set your own condition here.
+      // For example, only show available items.
+      return item.available;
     },
   },
 };
 </script>
-
-<style>
-
-.theme-dark {
-  background-color: #1C1D20;
-  color: #FFFFFF
-}
-
-.theme-white {
-  background-color: #1C1D20;
-  color: #FFFFFF;
-
-}
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,35 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
   theme: {
     extend: {
+      fontFamily :{
+        'sans': 'Sans-pro',
+      },
       colors: {
         dark: {
-          primary: "#1C1D20",
-          secondary: "#C559FF",
+          primary: "#291D32",
+          secondary: "#CF75FF",
+          strong :'#1A1220',
+          light:'#44394D',
         },
         light: {
-          primary: "#C559FF", // pink
-          secondary: "#1C1D20", // grey
+          primary: "#CF75FF", // pink
+          secondary: "#291D32", // grey
+          strong: '#F5E3FF',
+          light :'#F7F1F5'
         },
       },
     },
   },
-  conntent: [
-    "components/**/*.{vue,js,ts}",
-    "layouts/**/*.vue",
-    "pages/**/*.vue",
-    "composables/**/*.{js,ts}",
-    "plugins/**/*.{js,ts}",
-    "App.{js,ts,vue}",
-    "app.{js,ts,vue}",
-    "Error.{js,ts,vue}",
-    "error.{js,ts,vue}",
-    "content/**/*.md",
-  ],
-  plugins: [require("@tailwindcss/forms")],
-};
+  plugins: [],
+  
+}
 
